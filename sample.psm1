6755239ac3f331346b4c17d6cd1dcfbd3cfdfbce
A-n-g-e-l-o <angelo_as@hotmail.com>
2026-02-05 10:36:43 +0100

    Create sample.psm1 with exam questions

---
+++ sample.psm1
@@ -0,0 +1,46 @@
+PSM1 – exam questions
+Number : 000-000
+Passing Score: 850
+Time Limit: 60 min
+File Version: 2.0
+----------------------- Page 1-----------------------
+QUESTION 1
+A Scrum Team is only allowed to meet with stakeholders during Sprint Review.
+Choose one answer.
+A. True
+B. False
+Correct Answer: B
+QUESTION 2
+Which outcome is expected as Scrum Teams mature?
+Choose one answer.
+A. They will improve their definition of "Done" to include more stringent criteria.
+B. Sprint Reviews will no longer be needed.
+C. The Sprint Retrospectives will grow to be longer than 4 hours.
+D. A Scrum Master is no longer needed since they are mature a team now.
+E. There is no need for a time-boxed Sprint, since time-boxes are only for new Scrum Teams.
+Correct Answer: A
+QUESTION 3
+Sprint burndown charts are an efficient tracking tool because they show:
+Choose one answer.
+A. How much effort has gone into a Sprint.
+B. An estimate of the total work remaining for the Sprint.
+C. How many hours have been worked by each Development Team member.
+D. How many Product Backlog items remain.
+Correct Answer: B
+QUESTION 4
+When many Development Teams are working on a single product, what best describes the definition of "Done?"
+Choose one answer.
+A. Each Development Team defines and uses its own. The differences are discussed and reconciled during a hardening Sprint.
+B. All Development Teams must have a definition of "Done" that makes their combined work potentially releasable.
+C. Each Development Teams uses its own but must make their definition clear to all other Teams so the differences are known.
+D. It depends.
+Correct Answer: B
+QUESTION 5
+At the end of a Sprint a Product Backlog item worked on during the Sprint does not meet the definition of "Done".
+What two things should happen with the undone Product Backlog item?
+Choose 2 answers.
+A. Put it on the Product Backlog for the Product Owner to decide what to do with it.
+B. Do not include the item in the Increment the Sprint.
+C. Review the item, add the "Done" part of the estimate to the velocity and create a Story for the remaining work.
+D. If the stakeholders agree, the Product Owner can accept it and release it to the users.
+Correct Answer: AB
